838d532c6c8a65397b15db7ace1e13d90d53cdb0
Ben Foster <benfoster88@hotmail.com>
2024-07-04 21:59:10 +0100

    Initial commit

---
+++ test/ecs-service-connect-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsServiceConnectDemo from '../lib/ecs-service-connect-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-service-connect-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsServiceConnectDemo.EcsServiceConnectDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
